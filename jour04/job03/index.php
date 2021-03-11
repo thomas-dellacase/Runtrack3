a7a69709f753016b1e20ccfcb891b4ce6cf80801
thomas-dellacase <thomas.dellacase@laplatefrome.io>
2021-03-11 21:49:01 +0100

    ok

--- jour04/job03/index.php
+++ jour04/job03/index.php
@@ -1,36 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 <body>
-    <form>
-        <input type='text' placeholder='id'></input>
-        <input type='text' placeholder='nom'></input>
-        <select>
-            <option valeur="Grass">Plante</option>
-            <option valeur="Fighting">Combat</option>
-            <option valeur="Dragon">Dragon</option>
-            <option valeur="Water">Eau</option>
-            <option valeur="Electric">Electrique</option>
-            <option valeur="Fire">Feu</option>
-            <option valeur="Ice">Glace</option>
-            <option valeur="Bug">Insectes</option>
-            <option valeur="Normal">Normale</option>
-            <option valeur="Poison">Plante</option>
-            <option valeur="Psy">Psy</option>
-            <option valeur="Rock">Pierre</option>
-            <option valeur="Ground">Sol</option>
-            <option valeur="Gost">Spectre</option>
-            <option valeur="Flying">Vol</option>
+    <form method="post" class="submit">
+        <input type="text" name="id" placeholder="ID">
+        <input type="text" name="nom" placeholder="Nom">
+        <select name="type">
+            <option value="Grass">Grass</option>
+            <option value="Poison">Poison</option>
+            <option value="Fire">Fire</option>
+            <option value="Flying">Flying</option>
+            <option value="Water">Water</option>
+            <option value="Bug">Bug</option>
+            <option value="Normal">Normal</option>
+            <option value="Electric">Electric</option>
+            <option value="Ground">Ground</option>
+            <option value="Fairy">Fairy</option>
+            <option value="Fighting">Fighting</option>
+            <option value="Psychic">Psychic</option>
+            <option value="Rock">Rock</option>
+            <option value="Steel">Steel</option>
+            <option value="Ice">Ice</option>
+            <option value="Ghost">Ghost</option>
+            <option value="Dragon">Dragon</option>
         </select>
-
+        <button type="submit">Filtrer</button>
     </form>
-    <button id="bouton" onclick="jsonValueskey(myObj,'type')">Filtrer</button>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <div class="content">
+    </div>
     <script src="script.js"></script>
-</body>    
-</html> 
+</body>
+</html>
--- jour04/job03/script.js
+++ jour04/job03/script.js
@@ -1,23 +1,77 @@
 $(document).ready(function() {
-    $("#bouton").click(function(){
+    $('.submit').submit(function() {
+        var form = $(this).serializeArray();
+
         $.ajax({
-            method : "GET",
-            url : "pokemon.json",
-            data: {id:""},
+            url : 'pokemon.json',
+            method : 'post',
+            dataType : 'json',
+            data : form,
+            success : (date) => {
+                let filter = date.filter(function (i){
+                    if((form[0].value.length && i.id !== parseInt(form[0].value)))
+                    {
+                        return false;
+                    }
+                    if(form[1].value.length && !Object.values(i.name).includes(form[1].value))
+                    {
+                        return false;
+                    }
+                    if(!Object.values(i.type).includes(form[2].value))
+                    {
+                        return false;
+                    }
+                    return true;
+                });
 
-            done: function(data){
-                $("#id").value=data;
-                
-                function jsonValueskey(myJSON,id,nom,type){
-                    var myJSON = "{'id':'1', 'nom':'Bulbizarre', 'type':'Grass'}";
-                    const myObj = JSON.parse(myJSON);
-                    console.log(myJSON.id);
+                appendFilter(filter)
 
-                    $("#bouton").html("id");
-                    return myJSON[type]
-                }
-            }
- 
-         })
+            },
+        });
+        return false;
     })
 });
+function appendFilter(filter) {
+    let table = $('<table></table>');
+    let head = $('<thead></thead>');
+    let td = $('<td></td>');
+    let body = $('<tbody></tbody>');
+    head.append(td)
+    table.append(head);
+    table.append(body)
+    $("body .content").html(table)
+    let tr = "";
+    filter.forEach(function (object) {
+         tr += "<tr>" +
+            "<td>" +
+            object.id +
+        "</td>"   +
+        "<td>" +
+        getValueByObject(object.name) +
+        "</td>" +
+            "<td>" +
+            getValueByArray(object.type) +
+            "</td>" +
+            "<td>" +
+            getValueByObject(object.base) +
+            "</td>" +
+        "</tr>";
+
+    });
+    $('table tbody').html(tr);
+}
+function getValueByObject(object) {
+    let output = '';
+    let array = Object.entries(object);
+    array.forEach((value) => {
+        output += value[0] +" : "+ value[1]
+    });
+    return output;
+}
+function getValueByArray(array) {
+    let output = '';
+    array.forEach(function(value)  {
+        output += value
+    });
+    return output;
+}
