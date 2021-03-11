52aecea9508ba1fa1e38aa4b49e6bcfbaee4d5f3
thomas-dellacase <thomas.dellacase@laplatefrome.io>
2021-03-11 16:22:47 +0100

    JOB 2 JOURS 3

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -4,18 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <title>Document</title>
 </head>
 <body>
 <main>
     <div id="melangees">
-        <img src="arc1.png">
-        <img src="arc2.png">
-        <img src="arc3.png">
-        <img src="arc4.png">
-        <img src="arc5.png">
-        <img src="arc6.png">
+        <img id="arc1" src="arc1.png">
+        <img id="arc2" src="arc2.png">
+        <img id="arc3" src="arc3.png">
+        <img id="arc4" src="arc4.png">
+        <img id="arc5" src="arc5.png">
+        <img id="arc6" src="arc6.png">
     </div>
     <div id="rangees"></div>
     <button id="button">Mélanger</button>
@@ -23,4 +24,3 @@
 <script src="script.js"></script>
 </body>    
 </html> 
-
--- jour03/job02/script.js
+++ jour03/job02/script.js
@@ -9,5 +9,44 @@ $(document).ready(function(){
             parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
         }
     })
-    
-})
+
+    $("img").click(function(){
+        $(this).appendTo("#rangees");
+        var count = $("#rangees img").length;
+        console.log(count);
+        tableau.push($(this).attr('id'));
+        console.log(tableau);
+        console.log(tableau2);
+
+        var i = 1;
+        if(count == 6){
+            var gagne= true;
+
+            while(i < 6){
+
+                if(tableau[i] != tableau2[i]){
+                    gagne=false;
+                }
+            i++;
+            }
+
+            if(gagne){
+                var p = document.createElement('p');
+                var newelement = document.createTextNode("Vous avez gagné!");
+                p.setAttribute("id","gagne");
+
+                document.body.appendChild(p);
+                p.appendChild(newelement);
+            }
+
+            else{
+                var p = document.createElement('p');
+                var newelement = document.createTextNode("Vous avez perdu!");
+                p.setAttribute("id","perdu");
+
+                document.body.appendChild(p);
+                p.appendChild(newelement);
+            }
+        }
+    });   
+});
