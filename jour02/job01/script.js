30716bce549d9ae84e998490ad605308eef6cec8
thomas-dellacase <thomas.dellacase@laplateforme.io>
2022-03-15 16:48:53 +0100

    job2

---
+++ jour02/job01/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('coucou');
+    
+    let bouton = document.getElementById('button');
+    
+    bouton.addEventListener('click', event => {
+    
+    let citation = document.getElementById('citation');
+    console.log(citation.innerHTML);
+    
+    
+    })
+    
+    })
---
+++ jour02/job02/script.js
@@ -0,0 +1,19 @@
+'use strict';
+document.addEventListener("DOMContentLoaded",() =>{
+    
+    function showhide(){
+        let doc = document.getElementById('citation');
+        if(doc.style.display === 'none'){
+            doc.style.display = 'block';
+        }else{
+            doc.style.display = 'none';
+        }
+    }
+
+    let button =document.getElementById("button");
+
+    button.addEventListener("click",() =>{
+        showhide();
+    })
+
+})
