53083562c2aa1e354086abdb6824e9df808338fe
thomas-dellacase <thomas.dellacase@laplateforme.io>
2022-03-17 08:47:20 +0100

    jour3 job03 = la depression

---
+++ jour03/job01/script.js
@@ -0,0 +1,9 @@
+    $(document).ready(function(){
+            $("h1").hide();
+            $("#hide").click(function(){
+                $("html").hide();
+            })
+        $("#show").click(function(){
+            $("h1").show();
+        });
+    });
